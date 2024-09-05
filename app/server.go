8b4358f610af9464539cd6c0e3e7abdf3dd50d53
sscimff <wtc9199@gmail.com>
2024-09-05 14:33:57 -0700

    pass 1st stage modify

--- app/server.go
+++ app/server.go
@@ -11,4 +11,14 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
