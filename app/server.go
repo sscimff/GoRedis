9e50f701b851d631d87b16b78498079fe043eca7
sscimff <wtc9199@gmail.com>
2024-09-07 23:03:54 -0700

    Respond to multiple PINGs

--- app/server.go
+++ app/server.go
@@ -6,20 +6,31 @@ import (
 	"os"
 )
 
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error accepting connection:", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("+PONG\r\n"))
 	defer conn.Close()
+
+	// Get the incoming data
+	buf := make([]byte, 128)
+
+	for {
+		// Read from the connection
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection:", err.Error())
+			return
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
