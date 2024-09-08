457e3ec88622672f8481f05e06fe28da86003352
sscimff <wtc9199@gmail.com>
2024-09-08 00:40:43 -0700

    Handle concurrent clients

--- app/server.go
+++ app/server.go
@@ -12,25 +12,26 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	fmt.Println("Server is listening on port 6379...")
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection:", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
-
-	// Get the incoming data
-	buf := make([]byte, 128)
-
+	// Keep accepting incoming connections
 	for {
-		// Read from the connection
-		_, err := conn.Read(buf)
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error reading from connection:", err.Error())
-			return
+			fmt.Println("Error accepting connection:", err.Error())
+			os.Exit(1)
 		}
 
+		// Using goroutine to handle each client connection separately
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	for {
+		// Create a buffer to read the client's command
+		buf := make([]byte, 128)
+		conn.Read(buf)
 		conn.Write([]byte("+PONG\r\n"))
 	}
 }
